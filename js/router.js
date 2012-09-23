7bba6b1a095ff8a352d0f4fe7253da76d5844a51
Adam Haley <adamhaley@Adams-MacBook-Pro.local>
2012-09-22 22:06:03 -0700

    Added styles to boxslider, played with settings, got a reasonably good flow going.

--- js/router.js
+++ js/router.js
@@ -24,12 +24,13 @@ define([], function() {
     	}
 		, defaultAction: function( actions ){
             $('#content nav').html('');
+             $('#gallery').fadeOut(); 
 		}
         , showInfo: function(which){
             console.log('info ' + which);
 
             $('#content nav').html('');
-
+             $('#gallery').html(''); 
             var navFile = 'templates/nav-' + which + '.tpl';
         
             $.get(navFile,function(data){
@@ -40,16 +41,19 @@ define([], function() {
         , showGallery: function(cat){
              $('#content nav').html('');
             // console.log('gallery ' + cat);
+            var loadGallery = function(){
+                if(!$('#gallery').is(':visible')){
+                    $('#gallery').fadeIn();
+                }
 
-            $.ajax({
+                $.ajax({
                 url:'/graphoart/images/gallery/' + cat + '/'
                 , error: function(data){
                     console.log(data);
                 }
                 , success: function(data){
-                    
-                    $('#gallery').html(''); 
-
+            
+                    console.log('ajax call successful');
                     var galleryHtml = '<ul id="carousel">';
                     var images = $(data)[5];
 
@@ -58,16 +62,33 @@ define([], function() {
                         if(entry != 'Parent Directory'){    
                             galleryHtml += '<li><img src="images/gallery/' + cat + '/' + entry + '"" /></li>"';       
                         }
-                        
                     });
 
                     galleryHtml += '</ul>';
 
-                    $('#gallery').html(galleryHtml); 
-                    $('#carousel').bxSlider();
+                    var options = {
+                        // ticker: true
+                        // , tickerSpeed: 1050
+                        displaySlideQty: 3
+                        , moveSlideQty: 2
+                        , auto: true
+                    }
+
+                    //figure out effects queue here
+                    // $('#gallery').fadeOut().html(galleryHtml).fadeIn();
+                    $('#gallery').html(galleryHtml);
+                    $('#carousel').bxSlider(options);
+                    // $('#gallery').show(200);
+                    console.log('done');
                 }
 
-            });
+                });
+
+            };
+
+            // $('#gallery').fadeOut(200, function(){loadGallery();});
+            loadGallery()
+            
         }
 	})
     , router = new AppRouter;
