a165c5d09a83fa582bedd8013a5cb511e8cbe6a5
Adam Haley <adamhaley@Adams-MacBook-Pro.local>
2012-09-22 20:43:41 -0700

    Added gallery directories and photos. Created directory reading mechanism that loads the photos into the galleries

--- js/router.js
+++ js/router.js
@@ -28,7 +28,10 @@ define([], function() {
         , showInfo: function(which){
             console.log('info ' + which);
 
+            $('#content nav').html('');
+
             var navFile = 'templates/nav-' + which + '.tpl';
+        
             $.get(navFile,function(data){
                 $('#content nav').html(data);
             });
@@ -36,8 +39,35 @@ define([], function() {
         }
         , showGallery: function(cat){
              $('#content nav').html('');
-            console.log('gallery ' + cat)
+            // console.log('gallery ' + cat);
 
+            $.ajax({
+                url:'/graphoart/images/gallery/' + cat + '/'
+                , error: function(data){
+                    console.log(data);
+                }
+                , success: function(data){
+                    
+                    $('#gallery').html(''); 
+
+                    var galleryHtml = '<ul>';
+                    var images = $(data)[5];
+
+                    $(images).find('a').each(function(){
+                        var entry = $(this).text().trim();
+                        if(entry != 'Parent Directory'){    
+                            galleryHtml += '<li><img src="images/gallery/' + cat + '/' + entry + '"" /></li>"';       
+                        }
+                        
+                    });
+
+                    galleryHtml += '</ul>';
+
+                    $('#gallery').html(galleryHtml); 
+                    
+                }
+
+            });
         }
 	})
     , router = new AppRouter;
